b48269b93f5a8110d621ab420c6fd30de287d0a2
James Joslin <acquaintedmail@gmail.com>
2022-03-03 19:17:27 -0600

    troubleshooting blog

--- acquainted_website/settings.py
+++ acquainted_website/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True #config('DEBUG', default=True, cast=bool)
 
 
 
